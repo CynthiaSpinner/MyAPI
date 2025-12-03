1f987c50c8f696f4c9d2cc4facfb319bc0cfff06
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-12-02 18:55:13 -0600

    created db request log queries for creating logs with ip and get all logs

---
+++ db/requestLogQueries.js
@@ -0,0 +1,19 @@
+const { pool } = require('.connection');
+
+//log a new request
+const logRequest = async (method, endpoint, statusCode, ipAddress) => {
+    await pool.query(
+        'INSERT INTO request_logs (method, endpoint, status_code, ip_address) VALUES (?, ?, ?, ?)',
+        [method, endpoint, statusCode, ipAddress]
+    );
+};
+
+//get all logged requests
+const getAllRequestLogs = async () => {
+    const [rows] = await pool.query(
+        'SELECT * FROM request_logs ORDER BY timestamp DESC'
+    );
+    return rows;
+};
+
+module.exports = { logRequest, getAllRequestlogs };
